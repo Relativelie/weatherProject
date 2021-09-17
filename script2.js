889e2253c21c08208bc11df2ea38beb5acb667c4
Relativelie <sopio1992@gmail.com>
2021-09-17 22:19:57 +0300

    chart test in glitch

--- script2.js
+++ script2.js
@@ -168,3 +168,38 @@ function closeInput() {
     searchInput.value = "";
     searchInput.classList.remove("redBorder");
 }
+
+
+
+
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+
+  var myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
